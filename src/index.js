99dc64e7e48f30dea463fd9930d13ab336862e53
yar4ick <y.hapalov@gmail.com>
2022-02-17 23:46:32 +0200

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let closingBrackets = []
+
+  for(let element of str) 
+  {
+    if (!!(checkBracketType(element, bracketsConfig)) && !(closingBrackets.includes(element)))
+    {
+      closingBrackets.unshift(getClosingBracket(element, bracketsConfig));
+    }
+    else if (element === closingBrackets[0]) closingBrackets.shift()
+    else return false;
+  }
+
+  if (closingBrackets.length > 0) return false;
+
+  return true;
+}
+
+function getClosingBracket (char, bracketsConfig)
+{
+  for (let bracketPair of bracketsConfig)
+  {
+    if (bracketPair[0] === char) return bracketPair[1]
+  }
+}
+
+function checkBracketType (char, bracketsConfig)
+{
+  for (let bracketPair of bracketsConfig)
+  {
+    if (bracketPair[0] === char) return true;
+  }
+
+  return false;
 }
